77e39f3f76053b1fbe71f77a6b7288d6d3e8260c
Mark Palfreeman <markpalfreeman@gmail.com>
2016-04-04 14:31:08 -0700

    Move data, add, and delete to App level (where state lives)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header'
+import { notes } from './data/notes'
 
 const { any } = React.PropTypes
 
@@ -8,11 +9,41 @@ const App = React.createClass({
     children: any
   },
 
+  getInitialState () {
+    return {
+      notes: notes
+    }
+  },
+
+  addNote (note) {
+    this.state.notes.push(note)
+    this.setState({
+      notes: this.state.notes
+    })
+  },
+
+  deleteNote (noteId) {
+    let {notes} = this.state
+
+    notes = notes.filter((n) => (
+      // Get new array without ID match
+      noteId !== n.id
+    ))
+
+    this.setState({
+      notes: notes
+    })
+  },
+
   render () {
     return (
       <div>
         <Header/>
-        {this.props.children}
+        {React.cloneElement(this.props.children, {
+          notes: this.state.notes,
+          addNote: this.addNote,
+          deleteNote: this.deleteNote
+        })}
       </div>
     )
   }
