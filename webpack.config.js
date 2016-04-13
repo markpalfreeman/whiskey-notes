8ac818f56e92ed5970cf61fd7471e045df5f7c00
Mark Palfreeman <markpalfreeman@gmail.com>
2016-04-13 13:12:59 -0700

    Add 200.html for Surge client-side routing goodness

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,11 @@ var config = require('hjs-webpack')
 module.exports = config({
   in: 'src/index.js',
   out: 'public',
-  clearBeforeBuild: true
+  clearBeforeBuild: true,
+  html: function (data) {
+    return {
+      'index.html': data.defaultTemplate(),
+      '200.html': data.defaultTemplate()
+    }
+  }
 })
