4aa55406cd8cd327f33220067d35c33efe1cffee
Mark Palfreeman <markpalfreeman@gmail.com>
2018-01-10 20:46:26 -0800

    start over w/ create-react-app

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base'
-
-export default Rebase.createClass({
-  apiKey: 'AIzaSyCxUjim-n2kFbAxX2QApXjCr_PgaWVzfxQ',
-  authDomain: 'whiskey-notes.firebaseapp.com',
-  databaseURL: 'https://whiskey-notes.firebaseio.com',
-  storageBucket: 'whiskey-notes.appspot.com',
-  messagingSenderId: '32005084631'
-})
--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-require('./sass/app.sass')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import App from './components/App'
-import About from './pages/AboutPage'
-import NotesPage from './pages/NotesPage'
-import LoginPage from './pages/LoginPage'
-import Note from './components/Note'
-import FourOhFour from './pages/404'
-
-const Index = () => (
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={NotesPage} />
-      <Route path='about' component={About} />
-      <Route path='notes' component={NotesPage} />
-      <Route path='notes/:note' component={Note} />
-      <Route path='login' component={LoginPage} />
-      <Route path='*' component={FourOhFour} />
-    </Route>
-  </Router>
-)
-
-ReactDOM.render(<Index />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
