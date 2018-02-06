9e4c3a8cc719160ef4bd308889dc11c50bc40c33
Calvin Kelly <ckell87@gmail.com>
2018-02-06 15:51:04 -0800

    Updated NoteList to display list of Whiskeys and Note to show all properties

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
 
-function Note(props) {
-  return <div>{props.note.name}</div>
+function Note({ note }) {
+  return (
+    <form onSubmit="">
+      <label>
+        Name:
+        <input type="text" value={`${note.name}`} />
+      </label>
+      <label>
+        Distiller:
+        <input type="text" value={`${note.distiller}`} />
+      </label>
+      <label>
+        Age:
+        <input type="text" value={`${note.age}`} />
+      </label>
+      <label>
+        Category:
+        <select>
+          <option>{note.category}</option>
+        </select>
+      </label>
+      <label>
+        Color:
+        <input type="text" value={`${note.color}`} />
+      </label>
+      <label>
+        Country:
+        <input type="text" value={`${note.country}`} />
+      </label>
+      <label>
+        Rating:
+        <input type="number" value={`${note.rating}`} />
+      </label>
+      <label>
+        Tasting Notes:
+        <textarea cols="30" rows="10" value={`${note.tastingNotes}`} />
+      </label>
+    </form>
+  )
 }
 
 export default Note
--- src/components/NoteList/NoteList.jsx
+++ src/components/NoteList/NoteList.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function NoteList() {
-  return <div>NoteList</div>
+function NoteList({ notes }) {
+  return (
+    <div>
+      {notes.map(whiskey => (
+        <Link to={`/note/${whiskey.id}`}>
+          <div className="whiskey-card">
+            <h3>{whiskey.name}</h3>
+            <span className="whiskey-details">{whiskey.distiller}</span>
+            <span className="whiskey-details">&#8226;</span>
+            <span className="whiskey-details">{whiskey.category}</span>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 export default NoteList
